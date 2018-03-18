1ca6f7bac06ea0c9c4e5a5caf98f6756106f90dc
Alexander <ketov.alex.v@yandex.kz>
2018-03-18 16:21:05 +0600

    Code refactoring.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const constants = require('../constants');
-/** GEO_API_KEY - it's your API key which you can generate in the developer's account following this instruction:
- * 1. Visit the url above: https://console.developers.google.com/apis/credentials?project=_
- * 2. Create a new project (you'll need to create or sign in to a Google account)
- * 3. Visit "library" in the sidebar
- * 4. Search for "Google Maps Geocoding API" and click "Enable"
- * 5. This will bring you over to the library dashboard where you'll need to click "Create credential" (screenshot).
- * 6. Then click "What credential do I need?" (screenshot). That'll spit out an API key. This is what you need.
- * 7. Change the request URL by adding a "key" query string. That would be something like the following. Note I've added "&" to separate the key value pairs.
- * They new key value pair is "key" for the key and the API Key from Google for the value.
- * https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=YOURKEYHERE
- * 8. Everything should work.
- * or you can simply remove &key=${constants.GEO_API_KEY} and use URL with limit on requests per day
- */
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -28,19 +14,11 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-const encodedAddress = encodeURIComponent(argv.address);
 
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${constants.GEO_API_KEY}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google service.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, null, 2));
     }
 });
