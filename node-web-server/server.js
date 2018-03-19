6a2610f3cd6060913a2a452c9df530c8aa957289
alexander_ketov <alexander_ketov@epam.com>
2018-03-19 08:15:13 +0600

    Add dynamic templates with handlebars.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Alexandr',
-        likes: [
-            'Cycling',
-            'Reading'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
