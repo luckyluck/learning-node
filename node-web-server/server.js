d819cbf1c5d1139eeb4358d227e4feb2f0b9a79f
alexander_ketov <alexander_ketov@epam.com>
2018-03-19 08:40:54 +0600

    Express middleware.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', error => {
+        if (error) {
+            console.log('Unable to append to server,log.');
+        }
+    });
+
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname + '/public'));
-
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt', text => {
-    return text.toUpperCase();
-});
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
