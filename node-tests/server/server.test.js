2123f88f5ffe8c4ed82be8fbb1e4bf567f294d08
Alexander <ketov.alex.v@yandex.kz>
2018-03-21 20:54:17 +0600

    Small tests refactoring.

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,25 +3,31 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world response', done => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect(res => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', done => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect(res => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('should return array of users', done => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect(res => {
-            expect(res.body.length).toBe(3);
-            expect(res.body).toInclude({ name: 'Alexandr', age: 27 });
-        })
-        .end(done);
+    describe('GET /users', () => {
+        it('should return array of users', done => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect(res => {
+                    expect(res.body.length).toBe(3);
+                    expect(res.body).toInclude({ name: 'Alexandr', age: 27 });
+                })
+                .end(done);
+        });
+    });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,45 +2,50 @@ const utils = require('./utils');
 // const assert = require('assert');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-    // assert.equal(utils.add(33, 11), 44);
-    expect(utils.add(33, 11)).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-it('it should async add two numbers', done => {
-    utils.asyncAdd(33, 11, (summ) => {
-        expect(summ).toBe(44).toBeA('number');
-        done();
+    // describe('#add', () => {
+    it('should add two numbers', () => {
+        // assert.equal(utils.add(33, 11), 44);
+        expect(utils.add(33, 11)).toBe(44).toBeA('number');
     });
-});
 
-it('should multiple number', () => {
-    expect(utils.square(9)).toBe(81).toBeA('number');
-});
-
-it('should async multiple number', done => {
-    utils.asyncSquare(9, (summ) => {
-        expect(summ).toBe(81).toBeA('number');
-        done();
+    it('it should async add two numbers', done => {
+        utils.asyncAdd(33, 11, (summ) => {
+            expect(summ).toBe(44).toBeA('number');
+            done();
+        });
     });
-});
+    // });
 
-it('should expect some values', () => {
-    expect(utils.add(33, 11)).toBeMoreThan(43);
-    expect(utils.add(33, 11)).toNotBe(45);
-    expect({ name: 'Alexandr' }).toEqual({ name: 'Alexandr' });
-    expect([2, 3, 4]).toInclude(2);
-    expect([2, 3, 4]).toExclude(5);
-    expect({
-        name: 'Alexandr',
-        age: 27,
-        location: 'Philadelphia'
-    }).toInclude({ age: 27 });
-    expect({
-        name: 'Alexandr',
-        age: 27,
-        location: 'Philadelphia'
-    }).toExclude({ age: 25 });
+    it('should multiple number', () => {
+        expect(utils.square(9)).toBe(81).toBeA('number');
+    });
+
+    it('should async multiple number', done => {
+        utils.asyncSquare(9, (summ) => {
+            expect(summ).toBe(81).toBeA('number');
+            done();
+        });
+    });
+
+    it('should expect some values', () => {
+        expect(utils.add(33, 11)).toBeMoreThan(43);
+        expect(utils.add(33, 11)).toNotBe(45);
+        expect({ name: 'Alexandr' }).toEqual({ name: 'Alexandr' });
+        expect([2, 3, 4]).toInclude(2);
+        expect([2, 3, 4]).toExclude(5);
+        expect({
+            name: 'Alexandr',
+            age: 27,
+            location: 'Philadelphia'
+        }).toInclude({ age: 27 });
+        expect({
+            name: 'Alexandr',
+            age: 27,
+            location: 'Philadelphia'
+        }).toExclude({ age: 25 });
+    });
 });
 
 it('should expect correct first and last names', () => {
