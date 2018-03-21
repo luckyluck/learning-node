8da63aeb2ff6d98bd8991e602ef6201ff5b5add0
Alexander <ketov.alex.v@yandex.kz>
2018-03-21 19:48:09 +0600

    Test async functions.

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,5 +1,18 @@
 const add = (a, b) => a + b;
+
+const asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+};
+
 const square = a => a * a;
+
+const asyncSquare = (a, callback) => {
+    setTimeout(() => {
+        callback(a * a);
+    }, 1000);
+};
 
 const setName = (user, fullName) => {
     const nameAsArray = fullName.split(' ');
@@ -9,6 +22,8 @@ const setName = (user, fullName) => {
 
 module.exports = {
     add,
+    asyncAdd,
     square,
+    asyncSquare,
     setName
 };
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,28 @@
 const utils = require('./utils');
-const assert = require('assert');
+// const assert = require('assert');
 const expect = require('expect');
 
 it('should add two numbers', () => {
-    assert.equal(utils.add(33, 11), 44);
+    // assert.equal(utils.add(33, 11), 44);
     expect(utils.add(33, 11)).toBe(44).toBeA('number');
 });
 
+it('it should async add two numbers', done => {
+    utils.asyncAdd(33, 11, (summ) => {
+        expect(summ).toBe(44).toBeA('number');
+        done();
+    });
+});
+
 it('should multiple number', () => {
-    assert.equal(utils.square(9), 81);
     expect(utils.square(9)).toBe(81).toBeA('number');
+});
+
+it('should async multiple number', done => {
+    utils.asyncSquare(9, (summ) => {
+        expect(summ).toBe(81).toBeA('number');
+        done();
+    });
 });
 
 it('should expect some values', () => {
