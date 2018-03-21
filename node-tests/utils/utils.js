c05b86e3dbd8c89f90da8d306f2a20458c96809a
Alexander <ketov.alex.v@yandex.kz>
2018-03-21 16:46:09 +0600

    Add expect dependency and write some tests.

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,7 +1,14 @@
 const add = (a, b) => a + b;
 const square = a => a * a;
 
+const setName = (user, fullName) => {
+    const nameAsArray = fullName.split(' ');
+    user.firstName = nameAsArray[0];
+    user.lastName = nameAsArray[1];
+};
+
 module.exports = {
     add,
-    square
+    square,
+    setName
 };
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,10 +1,39 @@
 const utils = require('./utils');
 const assert = require('assert');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     assert.equal(utils.add(33, 11), 44);
+    expect(utils.add(33, 11)).toBe(44).toBeA('number');
 });
 
 it('should multiple number', () => {
     assert.equal(utils.square(9), 81);
+    expect(utils.square(9)).toBe(81).toBeA('number');
+});
+
+it('should expect some values', () => {
+    expect(utils.add(33, 11)).toBeMoreThan(43);
+    expect(utils.add(33, 11)).toNotBe(45);
+    expect({ name: 'Alexandr' }).toEqual({ name: 'Alexandr' });
+    expect([2, 3, 4]).toInclude(2);
+    expect([2, 3, 4]).toExclude(5);
+    expect({
+        name: 'Alexandr',
+        age: 27,
+        location: 'Philadelphia'
+    }).toInclude({ age: 27 });
+    expect({
+        name: 'Alexandr',
+        age: 27,
+        location: 'Philadelphia'
+    }).toExclude({ age: 25 });
+});
+
+it('should expect correct first and last names', () => {
+    const user = { firstName: 'Nikolay', lastName: 'Velikii' };
+    utils.setName(user, 'Alexandr Ketov');
+    expect(user).toExclude({ firstName: 'Nikolay' });
+    expect(user).toInclude({ firstName: 'Alexandr', lastName: 'Ketov' });
+    expect(user).toExclude({ lastName: 'Velikii' });
 });
