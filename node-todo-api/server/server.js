466d8b3a377fcae730fe4cd0851783a70996538b
Alexander <ketov.alex.v@yandex.kz>
2018-03-24 21:10:50 +0600

    Add user model and add more stuff into the schema (types, validators).

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,24 +5,46 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-const newTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: new Date().getTime()
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
 });
 
-newTodo.save().then(doc => {
-    console.log('Saved todo: ', JSON.stringify(doc, null, 2));
-}, error => {
-    console.log('Unable to save todo: ', error);
-});
+// const newTodo = new Todo({
+//     text: '  Edit this video   '
+// });
+
+// newTodo.save().then(doc => {
+//     console.log('Saved todo: ', JSON.stringify(doc, null, 2));
+// }, error => {
+//     console.log('Unable to save todo: ', error);
+// });
+
+// const newUser = new User({
+//     email: ''
+// });
+
+// newUser.save().then(user => {
+//     console.log('Saved user: ', JSON.stringify(user, null, 2));
+// }, error => {
+//     console.log('Unable to save user: ', error);
+// });
