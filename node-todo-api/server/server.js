376efe5b9dfce3f09a3cb4a912e08c288c168eb0
Alexander <ketov.alex.v@yandex.kz>
2018-03-25 17:36:01 +0600

    Add GET /todos/:id.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -24,6 +25,24 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
         res.send({ todos });
+    }, error => {
+        res.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
     }, error => {
         res.status(400).send(error);
     });
