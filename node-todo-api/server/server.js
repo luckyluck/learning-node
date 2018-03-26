b46190eaaa63f62f638300d03404fa8888f3a90f
alexander_ketov <alexander_ketov@epam.com>
2018-03-26 08:44:33 +0600

    Add PATCH /todos/:id.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
-const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -64,6 +65,34 @@ app.delete('/todos/:id', (req, res) => {
     }, error => {
         res.status(400).send(error);
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(todo => {
+            if (!todo) {
+                res.status(404).send();
+            }
+
+            res.send({ todo });
+        })
+        .catch(error => {
+            res.status(400).send();
+        });
 });
 
 app.listen(3000, () => {
