474de0f442c9213f023e208ba7eea34ad06fe6ff
Alexander <ketov.alex.v@yandex.kz>
2018-03-27 22:19:57 +0600

    Setup separate test and development envs.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,6 +10,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -95,8 +98,8 @@ app.patch('/todos/:id', (req, res) => {
         });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {
