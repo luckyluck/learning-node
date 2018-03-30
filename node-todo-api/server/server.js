eb9e7f36107591667e1213937ed3ff200b8b5cb8
Alexander <ketov.alex.v@yandex.kz>
2018-03-30 22:25:18 +0600

    Add new dependencies for JWT.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,7 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
-const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
